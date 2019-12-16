7866b0b95437ba6664e6441df4a18cd465626542
debauchery1st <trevor.j.martin@gmail.com>
2019-12-16 15:39:35 -0500

    stretch 1 - refactor so that our code is DRY

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import {useParams} from 'react-router-dom';
 import axios from 'axios';
+import MovieCard from "./MovieCard";
+
 
 const Movie = (props) => {
   const params = useParams();
@@ -32,26 +34,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
-      <div className="save-button">Save</div>
+      <MovieCard title={movie.title} director={movie.director} metascore={movie.metascore} stars={movie.stars} />
+    <div className="save-button">Save</div>
     </div>
   );
 }
